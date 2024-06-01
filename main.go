02bcdb10d788252a268bb6240b02cc7ee7e108a4
kupuma-ru21 <kupumaru21@gmail.com>
2024-05-31 23:49:13 -0700

    chore: learn method

--- main.go
+++ main.go
@@ -5,17 +5,15 @@ import (
 	"time"
 )
 
-type myTime struct {
-	time.Time //anonymous field
-}
+type myTime time.Time
 
-func (t myTime) first3Chars() string {
-	return t.String()[0:3]
+func (t myTime) first3Chars() {
+	fmt.Println(t)
 }
 
 func main() {
-	m := myTime{time.Now()}
+	m := myTime(time.Now())
 	//calling existing String method on anonymous Time field
-	fmt.Println("Full time now:", m.String())
-	fmt.Println("First 3 chars:", m.first3Chars()) //calling myTime.first3Chars
+	// fmt.Println("Full time now:", m.String())
+	m.first3Chars()
 }
