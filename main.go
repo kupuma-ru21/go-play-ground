9f614914270cf3cabc75bdf5e2a16209b1f546e4
kupuma-ru21 <kupumaru21@gmail.com>
2024-05-31 23:35:50 -0700

    chore: learn method

--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import "fmt"
 
-// make struct here
-type MytStruct struct {
-	namedField float32
-	int
-	string
+type S struct {
+	a int
 }
 
+type SType S        // New type
+type SAlias = S     // Alias
+type IntType int    // New type
+type IntAlias = int // Alias
+
+func (recv S) print() { // function for type defined on type S
+	fmt.Printf("%t: %[1]v\n", S(recv))
+}
+func (recv SType) print() { // function for type defined on the basis of S
+	fmt.Printf("%t: %[1]v\n", S(recv))
+}
+
+func (recv SAlias) print() {
+	fmt.Printf("%t: %[1]v\n", recv)
+}
+
+func (recv IntType) print() { // function for type defined on type on the basis of int
+	fmt.Printf("%t: %[1]v\n", recv)
+}
+
+// func (recv IntAlias) print() {
+// 	fmt.Printf("%t: %[1]v\n", recv)
+// }
+
 func main() {
-	mystructValue := MytStruct{7.5, 10, "test"}
-	fmt.Println(mystructValue)
+	a := S{10}
+	a.print() // calling function from line 13
+	b := SType{20}
+	b.print() // calling function from line 16
+	c := SAlias{30}
+	c.print() // calling function from line 13
+	d := IntType(40)
+	d.print() // calling function from line 24
+	e := IntAlias(50)
+	e.print()
 }
