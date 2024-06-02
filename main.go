9182cf1aac0b14d9635165a9f67f7ec4e4e95537
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-01 18:40:09 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-/* basic data structure upon which we'll define methods */
-type employee struct {
-	salary float32
+import (
+	"fmt"
+	"strconv"
+)
+
+type TwoInts struct {
+	a int
+	b int
 }
 
-/*
-a method which will add a specified percent to an
+func main() {
+	two1 := new(TwoInts)
+	two1.a = 12
+	two1.b = 10
+	fmt.Printf("two1 is: %v\n", two1)  // output: two1 is: (12 / 10)
+	fmt.Println("two1 is:", two1)      // output: two1 is: (12 / 10)
+	fmt.Printf("two1 is: %T\n", two1)  // output: two1 is: *main.TwoInts
+	fmt.Printf("two1 is: %#v\n", two1) // output: &main.TwoInts{a:12, b:10}
+}
 
-	employees salary
-*/
-func (this *employee) giveRaise(pct float32) {
-	this.salary = this.salary * (1 + pct)
+func (tn *TwoInts) String() string {
+	return "(" + strconv.Itoa(tn.a) + " / " + strconv.Itoa(tn.b) + ")"
 }
