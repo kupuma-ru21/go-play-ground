a29c093b143c3a1f0e39602417edc63395a5de31
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-02 14:29:40 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,70 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"go-play-ground/mysort"
-)
+import "fmt"
 
-// sorting of slice of integers
-func ints() {
-	data := []int{74, 59, 238, -784, 9845, 959, 905, 0, 0, 42, 7586, -5467984, 7586}
-	a := mysort.IntSlice(data) //conversion to type IntSlice
-	mysort.Sort(a)
-	if !mysort.IsSorted(a) {
-		panic("fail")
-	}
-	fmt.Printf("The sorted array is: %v\n", a)
+type Square struct {
+	side float32
 }
 
-// sorting of slice of strings
-func strings() {
-	data := []string{"Monday", "Friday", "Tuesday", "Wednesday", "Sunday", "Thursday", "", "Saturday"}
-	a := mysort.StringSlice(data)
-	mysort.Sort(a)
-	if !mysort.IsSorted(a) {
-		panic("fail")
-	}
-	fmt.Printf("The sorted array is: %v\n", a)
+type Triangle struct {
+	base   float32
+	height float32
 }
 
-// a type which describes a day of the week
-type day struct {
-	num       int
-	shortName string
-	longName  string
+type AreaInterface interface {
+	Area() float32
 }
 
-type dayArray struct {
-	data []*day
-}
-
-func (p *dayArray) Len() int           { return len(p.data) }
-func (p *dayArray) Less(i, j int) bool { return p.data[i].num < p.data[j].num }
-func (p *dayArray) Swap(i, j int)      { p.data[i], p.data[j] = p.data[j], p.data[i] }
-
-// sorting of custom type day
-func days() {
-	Sunday := day{0, "SUN", "Sunday"}
-	Monday := day{1, "MON", "Monday"}
-	Tuesday := day{2, "TUE", "Tuesday"}
-	Wednesday := day{3, "WED", "Wednesday"}
-	Thursday := day{4, "THU", "Thursday"}
-	Friday := day{5, "FRI", "Friday"}
-	Saturday := day{6, "SAT", "Saturday"}
-	data := []*day{&Tuesday, &Thursday, &Wednesday, &Sunday, &Monday, &Friday, &Saturday}
-	a := dayArray{data}
-	mysort.Sort(&a)
-	if !mysort.IsSorted(&a) {
-		panic("fail")
-	}
-	for _, d := range data {
-		fmt.Printf("%s ", d.longName)
-	}
-	fmt.Printf("\n")
+type PeriInterface interface {
+	Area() float32
+	Perimeter() float32
 }
 
 func main() {
-	ints()
-	strings()
-	days()
+	aI := AreaInterface(&Triangle{base: 10, height: 5})
+	fmt.Printf("The triangle has area: %f\n", aI.Area())
+
+	pI := PeriInterface(&Square{side: 5})
+	fmt.Printf("The square has area: %f\n", pI.Area())
+	fmt.Printf("The square has perimeter: %f\n", pI.Perimeter())
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func (sq *Square) Perimeter() float32 { // implement method called on square to calculate its perimeter
+	return sq.side * 4
+}
+
+func (tr *Triangle) Area() float32 { // implement method called on triangle to calculate its area
+	return (tr.base * tr.height) / 2
 }
