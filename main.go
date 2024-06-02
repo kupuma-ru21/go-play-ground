402ac6781fd1db4e8ab72b432437b8e79bc43b0b
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-02 14:10:18 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,49 +1,47 @@
 package main
 
-type Simpler interface {
-	Get() int
-	Set(u int)
+import (
+	"fmt"
+)
+
+type List []int
+
+func (l List) Len() int { return len(l) }
+
+func (l *List) Append(val int) { *l = append(*l, val) }
+
+type Appender interface {
+	Append(int)
 }
 
-type Simple struct {
-	integer int
-}
-
-func (p *Simple) Get() int {
-	return p.integer
-}
-
-func (p *Simple) Set(u int) {
-	p.integer = u
-}
-
-type RSimple struct {
-	integer int
-}
-
-func (p *RSimple) Get() int {
-	return p.integer
-}
-
-func (p *RSimple) Set(u int) {
-	p.integer = u
-}
-
-func fI(it Simpler) int {
-	switch it.(type) {
-	case *Simple:
-		it.Set(5)
-		return it.Get()
-	case *RSimple:
-		it.Set(10)
-		return it.Get()
+func CountInto(a Appender, start, end int) {
+	for i := start; i <= end; i++ {
+		a.Append(i)
 	}
-	return 0
+}
+
+type Lener interface {
+	Len() int
+}
+
+func LongEnough(l Lener) bool {
+	return l.Len()*10 > 42
 }
 
 func main() {
-	sinple := Simpler(new(Simple))
-	rSinple := Simpler(new(RSimple))
-	println(fI(sinple))
-	println(fI(rSinple))
+	// A bare value
+	var lst List
+	// compiler error:
+	// cannot use lst (type List) as type Appender in function argument:
+	// List does not implement Appender (Append method requires pointer receiver)
+	// CountInto(lst, 1, 10)
+	if LongEnough(lst) { // VALID: Identical receiver type
+		fmt.Printf(" - lst is long enough\n")
+	}
+	// A pointer value
+	plst := new(List)
+	CountInto(plst, 1, 10) // VALID: Identical receiver type
+	if LongEnough(plst) {  // VALID: a *List can be dereferenced for the receiver
+		fmt.Printf(" - plst is long enough\n") // - plst2 is long enough
+	}
 }
