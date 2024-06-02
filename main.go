001e1c76706ce9ba1e2cda98a199460d0e3430bb
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-02 14:16:53 -0700

    chore: wip

--- main.go
+++ main.go
@@ -2,46 +2,69 @@ package main
 
 import (
 	"fmt"
+	"go-play-ground/mysort"
 )
 
-type List []int
-
-func (l List) Len() int { return len(l) }
-
-func (l *List) Append(val int) { *l = append(*l, val) }
-
-type Appender interface {
-	Append(int)
-}
-
-func CountInto(a Appender, start, end int) {
-	for i := start; i <= end; i++ {
-		a.Append(i)
+// sorting of slice of integers
+func ints() {
+	data := []int{74, 59, 238, -784, 9845, 959, 905, 0, 0, 42, 7586, -5467984, 7586}
+	a := mysort.IntSlice(data) //conversion to type IntSlice
+	mysort.Sort(a)
+	if !mysort.IsSorted(a) {
+		panic("fail")
 	}
+	fmt.Printf("The sorted array is: %v\n", a)
 }
 
-type Lener interface {
-	Len() int
+// sorting of slice of strings
+func strings() {
+	data := []string{"Monday", "Friday", "Tuesday", "Wednesday", "Sunday", "Thursday", "", "Saturday"}
+	a := mysort.StringSlice(data)
+	mysort.Sort(a)
+	if !mysort.IsSorted(a) {
+		panic("fail")
+	}
+	fmt.Printf("The sorted array is: %v\n", a)
 }
 
-func LongEnough(l Lener) bool {
-	return l.Len()*10 > 42
+// a type which describes a day of the week
+type day struct {
+	num       int
+	shortName string
+	longName  string
+}
+
+type dayArray struct {
+	data []*day
+}
+
+func (p *dayArray) Len() int           { return len(p.data) }
+func (p *dayArray) Less(i, j int) bool { return p.data[i].num < p.data[j].num }
+func (p *dayArray) Swap(i, j int)      { p.data[i], p.data[j] = p.data[j], p.data[i] }
+
+// sorting of custom type day
+func days() {
+	Sunday := day{0, "SUN", "Sunday"}
+	Monday := day{1, "MON", "Monday"}
+	Tuesday := day{2, "TUE", "Tuesday"}
+	Wednesday := day{3, "WED", "Wednesday"}
+	Thursday := day{4, "THU", "Thursday"}
+	Friday := day{5, "FRI", "Friday"}
+	Saturday := day{6, "SAT", "Saturday"}
+	data := []*day{&Tuesday, &Thursday, &Wednesday, &Sunday, &Monday, &Friday, &Saturday}
+	a := dayArray{data}
+	mysort.Sort(&a)
+	if !mysort.IsSorted(&a) {
+		panic("fail")
+	}
+	for _, d := range data {
+		fmt.Printf("%s ", d.longName)
+	}
+	fmt.Printf("\n")
 }
 
 func main() {
-	// A bare value
-	var lst List
-	// compiler error:
-	// cannot use lst (type List) as type Appender in function argument:
-	// List does not implement Appender (Append method requires pointer receiver)
-	// CountInto(lst, 1, 10)
-	if LongEnough(lst) { // VALID: Identical receiver type
-		fmt.Printf(" - lst is long enough\n")
-	}
-	// A pointer value
-	plst := new(List)
-	CountInto(plst, 1, 10) // VALID: Identical receiver type
-	if LongEnough(plst) {  // VALID: a *List can be dereferenced for the receiver
-		fmt.Printf(" - plst is long enough\n") // - plst2 is long enough
-	}
+	ints()
+	strings()
+	days()
 }
