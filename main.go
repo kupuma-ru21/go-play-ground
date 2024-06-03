0fd7d56a467d6c295a0140b27a7d0b2c243b1d8e
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-03 12:25:18 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"go-play-ground/mysort"
-)
+import "fmt"
+
+var i = 5
+var str = "ABC"
 
 type Person struct {
-	firstName string
-	lastName  string
+	name string
+	age  int
 }
 
-type Persons []Person
-
-func (p Persons) Len() int { return len(p) }
-
-func (p Persons) Less(i, j int) bool {
-	in := p[i].lastName + " " + p[i].firstName
-	jn := p[j].lastName + " " + p[j].firstName
-	return in < jn
-}
-
-func (p Persons) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
+type Any interface{} // empty interface
 
 func main() {
-	persons := Persons{
-		{"James", "Bond"},
-		{"Miss", "Moneypenny"},
-		{"Kevin", "Costner"},
+	var val Any
+	val = 5 // assigning integer to empty interface
+	fmt.Printf("val has the value: %v\n", val)
+	val = str // assigning string to empty interface
+	fmt.Printf("val has the value: %v\n", val)
+	pers1 := new(Person)
+	pers1.name = "Rob Pike"
+	pers1.age = 55
+	val = pers1 // assigning *Person type variable to empty interface
+	fmt.Printf("val has the value: %v\n", val)
+	switch t := val.(type) { // cases defined on type of val
+	case int: // if val is int
+		fmt.Printf("Type int %T\n", t)
+	case string: // if val is string
+		fmt.Printf("Type string %T\n", t)
+	case bool: // if val is bool
+		fmt.Printf("Type boolean %T\n", t)
+	case *Person: // if val is *Person
+		fmt.Printf("Type pointer to Person %T\n", *t)
+	default: // None of the above types
+		fmt.Printf("Unexpected type %T", t)
 	}
-	mysort.Sort(persons)
-	fmt.Println(persons)
 }
