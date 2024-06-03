599226a1cef43eb989dab0d7b06216773ba258f4
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-03 13:37:45 -0700

    chore: wip

--- main.go
+++ main.go
@@ -2,37 +2,30 @@ package main
 
 import "fmt"
 
-var i = 5
-var str = "ABC"
+type specialString string
 
-type Person struct {
-	name string
-	age  int
+var whatIsThis specialString = "hello"
+
+func TypeSwitch() {
+	testFunc := func(any interface{}) { // lambda function in combination with empty interface
+		switch v := any.(type) {
+		case bool: // if v is bool
+			fmt.Printf("any %v is a bool type", v)
+		case int: // if v is int
+			fmt.Printf("any %v is an int type", v)
+		case float32: // if v is float32
+			fmt.Printf("any %v is a float32 type", v)
+		case string: // if v is string
+			fmt.Printf("any %v is a string type", v)
+		case specialString: // if v is specialString
+			fmt.Printf("any %v is a special String!", v)
+		default: // none of types satisfied
+			fmt.Println("unknown type!")
+		}
+	}
+	testFunc(whatIsThis)
 }
 
-type Any interface{} // empty interface
-
 func main() {
-	var val Any
-	val = 5 // assigning integer to empty interface
-	fmt.Printf("val has the value: %v\n", val)
-	val = str // assigning string to empty interface
-	fmt.Printf("val has the value: %v\n", val)
-	pers1 := new(Person)
-	pers1.name = "Rob Pike"
-	pers1.age = 55
-	val = pers1 // assigning *Person type variable to empty interface
-	fmt.Printf("val has the value: %v\n", val)
-	switch t := val.(type) { // cases defined on type of val
-	case int: // if val is int
-		fmt.Printf("Type int %T\n", t)
-	case string: // if val is string
-		fmt.Printf("Type string %T\n", t)
-	case bool: // if val is bool
-		fmt.Printf("Type boolean %T\n", t)
-	case *Person: // if val is *Person
-		fmt.Printf("Type pointer to Person %T\n", *t)
-	default: // None of the above types
-		fmt.Printf("Unexpected type %T", t)
-	}
+	TypeSwitch()
 }
