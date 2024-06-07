381d8d2d47f5ac4d230d40d65c0be135a2dc5d66
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-07 15:07:25 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-type obj interface{}
+var inputReader *bufio.Reader
+var input string
+var err error
 
 func main() {
-	mf := func(i obj) obj {
-		switch i.(type) {
-		case int:
-			return i.(int) * 2
-		case string:
-			return i.(string) + i.(string)
-		}
-		return i
-	}
+	inputReader = bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter some input: ")
 
-	isl := []obj{0, 1, 2, 3, 4, 5}
-	res1 := mapFunc(mf, isl)
-	for _, v := range res1 {
-		fmt.Println(v)
+	input, err = inputReader.ReadString('\n')
+	if err == nil {
+		fmt.Printf("The input was: %s\n", input)
 	}
-	fmt.Println()
-
-	ssl := []obj{"0", "1", "2", "3", "4", "5"}
-	res2 := mapFunc(mf, ssl)
-	for _, v := range res2 {
-		fmt.Println(v)
-	}
-}
-
-func mapFunc(mf func(obj) obj, list []obj) []obj {
-	result := make([]obj, len(list))
-
-	for ix, v := range list {
-		result[ix] = mf(v)
-	}
-	return result
 }
