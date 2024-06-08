50b93dc720f7c299183097fe29c769ad824b12f8
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-08 13:03:02 -0700

    chore: wip

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import (
 )
 
 func main() {
-	inputFile := "products.txt"
-	buf, err := os.ReadFile(inputFile)
+	file, err := os.Open("products.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "File Error: %s\n", err)
+		panic(err)
 	}
-	fmt.Printf("%s\n", string(buf))
+	defer file.Close()
+	var col1, col2, col3 []string
+	for {
+		var v1, v2, v3 string
+		_, err := fmt.Fscanln(file, &v1, &v2, &v3) // scans until newline
+		if err != nil {
+			break
+		}
+		col1 = append(col1, v1)
+		col2 = append(col2, v2)
+		col3 = append(col3, v3)
+	}
+	fmt.Println(col1)
+	fmt.Println(col2)
+	fmt.Println(col3)
 }
