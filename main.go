081d8c994db8933d9c47d8ecabd68ecb04c922f8
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-08 14:31:31 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "os"
 
 func main() {
-	outputFile, outputError := os.OpenFile("output/output.dat", os.O_WRONLY|os.O_CREATE, 0666)
-	if outputError != nil {
-		fmt.Printf("An error occurred with file creation\n")
-		return
-	}
-	defer outputFile.Close()
-	for i := 0; i < 10; i++ {
-		fmt.Fprintf(outputFile, "Some test data.\n")
-	}
+	os.Stdout.WriteString("hello, world\n")
+	f, _ := os.OpenFile("output/output.dat", os.O_CREATE|os.O_WRONLY, 0)
+	defer f.Close()
+
+	f.WriteString("hello, world in a file\n")
 }
