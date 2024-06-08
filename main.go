63f507e2adcf568bf0f745b68994c2448b7075c6
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-08 15:12:12 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,60 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
-type Book struct {
-	title    string
-	price    float64
-	quantity int
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() {
+	filename := p.Title + ".txt"
+	err := os.WriteFile(filename, p.Body, 0600)
+	if err != nil {
+		fmt.Println("Error saving file: ", err)
+	}
+}
+
+func load(title string) *Page {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error loading file: ", err)
+	}
+	return &Page{Title: title, Body: body}
 }
 
 func main() {
-	bks := make([]Book, 1)
-	file, err := os.Open("products.txt")
-	if err != nil {
-		log.Fatalf("Error %s opening file products.txt: ", err)
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	for {
-		// read one line from the file:
-		line, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		// remove \r and \n so 2 in Windows, in Linux only \n, so 1:
-		line = string(line[:len(line)-1])
-		//fmt.Printf("The input was: -%s-", line)
-
-		strSl := strings.Split(line, ";")
-		book := new(Book)
-		book.title = strSl[0]
-		book.price, err = strconv.ParseFloat(strSl[1], 32)
-		if err != nil {
-			fmt.Printf("Error in file: %v", err)
-		}
-		//fmt.Printf("The quan was:-%s-", strSl[2])
-		book.quantity, err = strconv.Atoi(strSl[2])
-		if err != nil {
-			fmt.Printf("Error in file: %v", err)
-		}
-		if bks[0].title == "" {
-			bks[0] = *book
-		} else {
-			bks = append(bks, *book)
-		}
-	}
-	fmt.Println("We have read the following books from the file: ")
-	for _, bk := range bks {
-		fmt.Println(bk)
-	}
+	p1 := &Page{Title: "SamplePage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2 := load(p1.Title)
+	fmt.Println(string(p2.Body))
 }
