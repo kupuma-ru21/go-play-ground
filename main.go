80b3eaae29a555547f419ce66412f91a3d9ad8fb
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-08 19:19:17 -0700

    chore: wip

--- main.go
+++ main.go
@@ -5,31 +5,31 @@ import (
 	"fmt"
 )
 
-type Address struct {
-	Type    string
-	City    string
-	Country string
-}
-
-type VCard struct {
-	FirstName string
-	LastName  string
-	Addresses []*Address
-	Remark    string
-}
-
 func main() {
-	pa := &Address{"private", "Aartselaar", "Belgium"}
-	wa := &Address{"work", "Boom", "Belgium"}
-	vc1 := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "none"}
-	// fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}:
-	// JSON format:
-	js, _ := json.Marshal(vc1)
-	fmt.Printf("JSON format: %s", js)
-	vc2 := VCard{}
-	err := json.Unmarshal(js, &vc2)
+	b := []byte(`{"Name": "Wednesday", "Age": 6, "Parents": ["Gomez", "Morticia"]}`)
+	var f interface{}
+	err := json.Unmarshal(b, &f)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println(err)
 	}
-	fmt.Printf("Struct format: %v", vc2)
+	fmt.Println(f)
+	// fmt.Println(f["Name"]) // invalid operation: cannot index f (variable of type interface{})
+	m := f.(map[string]interface{})
+	fmt.Println("Name is", m["Name"])
+
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case float64:
+			fmt.Println(k, "is float64", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array:")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
 }
