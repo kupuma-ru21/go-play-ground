6f3f824c59dd079fdcbd446bc52c0d49d4ca3f15
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-09 16:25:37 -0700

    chore: wip

--- main.go
+++ main.go
@@ -2,30 +2,18 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
-	wg := new(sync.WaitGroup)
-	wg.Add(2)
-	ch := make(chan string)
-	go sendData(ch) // calling goroutine to send the data
-	go getData(ch)  // calling goroutine to receive the data
+	ch1 := make(chan int)
+	go pump(ch1)       // pump hangs
+	fmt.Println(<-ch1) // prints only 0
 	time.Sleep(1e9)
 }
 
-func sendData(ch chan string) { // sending data to ch channel
-	ch <- "Washington"
-	ch <- "Tripoli"
-	ch <- "London"
-	ch <- "Beijing"
-	ch <- "Tokyo"
-}
-
-func getData(ch chan string) {
-	for {
-		input := <-ch // receiving data sent to ch channel
-		fmt.Printf("%s ", input)
+func pump(ch chan int) {
+	for i := 0; ; i++ {
+		ch <- i
 	}
 }
