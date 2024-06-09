8767033e651e5ed3f3ef22605bf78b636d65c6ec
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-09 16:26:40 -0700

    chore: wip

--- main.go
+++ main.go
@@ -7,13 +7,19 @@ import (
 
 func main() {
 	ch1 := make(chan int)
-	go pump(ch1)       // pump hangs
-	fmt.Println(<-ch1) // prints only 0
+	go pump(ch1)
+	go suck(ch1)
 	time.Sleep(1e9)
 }
 
 func pump(ch chan int) {
-	for i := 0; ; i++ {
+	for i := 0; i < 10; i++ {
 		ch <- i
+	}
+}
+
+func suck(ch chan int) {
+	for {
+		fmt.Println(<-ch)
 	}
 }
