5c3164ea37595587dd0e0d57ac2b31acd1c492be
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-08 20:36:12 -0700

    chore: wip

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"encoding/gob"
+	"log"
+	"os"
 )
 
-type Person struct {
-	Name string `json:"personName"`
-	Age  int    `json:"personAge"`
+type Address struct {
+	Type    string
+	City    string
+	Country string
+}
+
+type VCard struct {
+	FirstName string
+	LastName  string
+	Addresses []*Address
+	Remark    string
 }
 
 func main() {
-	b := []byte(`{"personName": "Obama", "personAge": 57}`)
-	var p Person
-	// Unmarshalling
-	json.Unmarshal(b, &p)
-	fmt.Println(p)
-	// Marshalling
-	js, _ := json.Marshal(p)
-	fmt.Printf("%s\n", js)
+	pa := &Address{"private", "Aartselaar", "Belgium"}
+	wa := &Address{"work", "Boom", "Belgium"}
+	vc := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "none"}
+
+	// fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}:
+	// using an encoder:
+	file, _ := os.OpenFile("output/vcard.gob", os.O_CREATE|os.O_WRONLY, 0)
+	defer file.Close()
+	enc := gob.NewEncoder(file)
+	err := enc.Encode(vc)
+	if err != nil {
+		log.Println("Error in encoding gob")
+	}
 }
